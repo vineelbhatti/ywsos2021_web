ec6a7d933916ba10096868cc3e752d01ceecf7af
Snehil K <85982657+SneK152@users.noreply.github.com>
2021-07-22 20:58:59 -0700

    Frontend and backend fixes (#29)
    * Fixed responsiveness of 3 column section (homepage)
    
    * Added backend authentication checks to determine if login is incorrect and/or if email/username are in use when signing up to prevent duplicates
    
    * - When logged in, removed login and signup on 4 regular pages and replaced with "My account"
    - Added account settings menu (currently incomplete)
    - Fixed navbar on main.html
    
    * Added modal confirming deletion
    
    * - Fixed error with login button
    - Added logout button to all pages to make it more consistent (while logged in) so you don't have to go to account page to log out
    - Adding proper href to navbar-brand (Georepair title)
    - Small button fixes
    
    * Removing unnecessary passing of session variable

--- app.py
+++ app.py
@@ -110,16 +110,24 @@ def forum():
 def contact():
     return render_template('contact.html')
 
-@app.route('/main')
+@app.route('/main',methods=['GET','POST'])
 @login_required
 def main(user_id):
     users = db['users']
     user = users.find_one({'_id': bson.ObjectId(session['logged_in_id'])})
+    if request.method == "POST":
+        if request.form.get("changepass"):
+            print("hi")
+        if request.form.get("deleteacc"):
+            print("whassup")
+            users.remove({'_id': bson.ObjectId(session['logged_in_id'])})
+            return redirect("/logout")
     return render_template("main.html", user=user)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
+    error = False
     if login_form.validate_on_submit():
         users = db['users']
         result = users.find_one({
@@ -129,11 +137,15 @@ def login():
             session['logged_in'] = True
             session['logged_in_id'] = result['_id']
             return redirect('/main')
-    return render_template("login.html", login_form=login_form)
+        else:
+            error = True
+    return render_template("login.html", login_form=login_form, error=error)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     signup_form = SignupForm()
+    usererror = False
+    emailerror = False
     if signup_form.validate_on_submit():
         users = db['users']
         dt_now = datetime.now(tz=timezone.utc)
@@ -147,11 +159,16 @@ def signup():
             "latitude": location.latitude,
             "longitude": location.longitude
         }
-        users.insert_one(user)
-        session['logged_in'] = True
-        session['logged_in_id'] = user['_id']
-        return redirect('/main')
-    return render_template("signup.html", signup_form=signup_form)
+        if users.find_one({"username":user["username"]}) is not None:
+            usererror = True
+        elif users.find_one({"email":user["email"]}) is not None:
+            emailerror = True
+        else:
+            users.insert_one(user)
+            session['logged_in'] = True
+            session['logged_in_id'] = user['_id']
+            return redirect('/main')
+    return render_template("signup.html", signup_form=signup_form,usererror=usererror,emailerror=emailerror)
 
 @app.route('/logout')
 @login_required
